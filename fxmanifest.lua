a2099d705f19facdf2db7d9508965a572f16c946
hnt-neptunium <kw.chaney2003@gmail.com>
2021-12-06 22:29:14 -0600

    publish

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+version '2.0.0'
+author 'Cfx.re / hnt-neptunium'
+description 'FiveM loading screen edited from the base example.'
+
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'loadscreen.png'
+}
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+game 'gta5'
