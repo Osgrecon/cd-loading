e7fbe2cd26f2545c1241567fd85d634471db675f
hnt-neptunium <kw.chaney2003@gmail.com>
2021-12-28 16:24:05 -0600

    push 2.0

---
+++ nep-loading/fxmanifest.lua
@@ -0,0 +1,15 @@
+version '2.0.0'
+author 'neptunium'
+description 'This is a Cfx edit of the default resource for FiveM servers. Edited with love by Neptunium.'
+
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'loadscreen.png'
+}
+
+loadscreen 'index.html'
+
+fx_version 'cerulean'
+game 'gta5'
